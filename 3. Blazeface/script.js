727c317c2885334f55566eeb78014760e9dc502f
nessaaa <nnuranisahh@gmail.com>
2022-05-17 14:34:12 +0700

    Menyelesaikan blazeface untuk mendeteksi wajah digambar

--- 3. Blazeface/script.js
+++ 3. Blazeface/script.js
@@ -13,21 +13,44 @@ async function main() {
     // bounding boxes, probabilities, and landmarks, one for each detected face.
 
     const returnTensors = false; // Pass in `true` to get tensors back, rather than values.
-    const predictions = await model.estimateFaces(img, returnTensors);
+    const predictions = await model.estimateFaces(img, returnTensors); // script tensorflow hanya satu baris ini, coding selebihnya adalah manipulasi html
 
     ctx.drawImage(img, 0,0, img.width, img.height);
     console.log(predictions);
 
     // Jika ada wajah yang terdeteksi di dalam gambar
     if (predictions.length > 0) {
-        for (let i = 0; i < predictions.length; i++) {
-            const start = predictions[i].topLeft;
-            const end = predictions[i].bottomRight;
-            const size = [end[0] - start[0], end[1] - start[1]];
+        
+        // Baca semua wajah yang terdeteksi
+        predictions.forEach(element => {
+            console.log(element);
+            ctx.beginPath();
+            ctx.lineWidth = 4;
+            ctx.strokeStyle = "yellow";
+
+            ctx.rect(
+                element.topLeft[0], element.topLeft[1],
+                element.bottomRight[0] - element.topLeft[0],
+                element.bottomRight[1] - element.topLeft[1],
+            );
+            ctx.stroke();
+
+            // Menandai titik-tik landmarks
+            ctx.fillStyle = "yellow";
+            element.landmarks.forEach((landmark) => {
+                ctx.fillRect(landmark[0], landmark[1], 4, 4);
+            });
+        });
+
+//-------------codingan dari dokumentasi Blazeface Github---------------
+        // for (let i = 0; i < predictions.length; i++) {
+        //     const start = predictions[i].topLeft;
+        //     const end = predictions[i].bottomRight;
+        //     const size = [end[0] - start[0], end[1] - start[1]];
     
-            // Render a rectangle over each detected face.
-            ctx.fillRect(start[0], start[1], size[0], size[1]);
-        }
+        //     // Render a rectangle over each detected face.
+        //     ctx.fillRect(start[0], start[1], size[0], size[1]);
+        // }
         }
     }
     
